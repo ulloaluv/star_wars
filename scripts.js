44f577fcd11003be4843663bf32393551e46e79c
ulloaluv <135282123+ulloaluv@users.noreply.github.com>
2023-10-12 23:07:20 -0500

    Star Wars

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const API = 'https://swapi.dev/api/people/'
+
+function personaje(texto,p_id) {
+    let div = document.createElement('div')
+    let h1_texto = document.createTextNode('( '+p_id+' ) '+texto.name)
+    let h1 = document.createElement('h1')
+    h1.appendChild(h1_texto)
+    div.appendChild(h1)
+
+    let contenedor = document.getElementById('contenedor')
+    contenedor.appendChild( div )
+}
+
+async function obtener_personaje(id) {
+    try {
+        let response = await fetch(`${API}${id}`)
+        return personaje( await response.json(), await id)
+    } catch(error) {
+        console.error('[error]: ${error}')
+    }
+}
+
+for (let i=1; i<=80; i++) {
+    obtener_personaje(i)
+}
